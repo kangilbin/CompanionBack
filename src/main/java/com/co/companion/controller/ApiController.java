ae60d11802963ba6a2893db4f58b862f8eccedaf
kangilbin <kib3388@naver.com>
2023-02-12 09:06:38 +0900

    수정

--- src/main/java/com/co/companion/controller/ApiController.java
+++ src/main/java/com/co/companion/controller/ApiController.java
@@ -1,20 +1,15 @@
 package com.co.companion.controller;
 
-import com.co.companion.dto.ResponseDTO;
 import com.co.companion.dto.YouTubeDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.RequestEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-import java.net.URI;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.web.reactive.function.client.WebClient;
+
 
 @Slf4j
 @RestController
@@ -26,28 +21,32 @@ public class ApiController {
     @Value("${YOUTUBE-KEY}")
     private String YOUTUBE_KEY;
 
+
     // 유튜브 데이터 가져오기
     @GetMapping("/youtube/{type}")
     public ResponseEntity<?> youTubeList(@PathVariable(required = false) String type, @RequestBody YouTubeDTO youTubeDTO) {
         try {
-            URI uri = UriComponentsBuilder
-                    .fromUriString(YOUTUBE_URL)
-                    .path("/" + type)
-                    .queryParam("part", youTubeDTO.getPart())
-                    .queryParam("chart", youTubeDTO.getChart())
-                    .queryParam("regionCode", youTubeDTO.getRegionCode())
-                    .queryParam("maxResults", youTubeDTO.getMaxResults())
-                    .queryParam("videoCategoryId", youTubeDTO.getVideoCategoryId())
-                    .queryParam("key", YOUTUBE_KEY)
-                    .encode()
-                    .build()
-                    .toUri();
+
+            WebClient webClient = WebClient
+                    .builder()
+                    .baseUrl(YOUTUBE_URL)
+                    .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .build();
+
+            ResponseEntity<String> response = webClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/" + type)
+                            .queryParam("part", youTubeDTO.getPart())
+                            .queryParam("chart", youTubeDTO.getChart())
+                            .queryParam("regionCode", youTubeDTO.getRegionCode())
+                            .queryParam("maxResults", youTubeDTO.getMaxResults())
+                            .queryParam("videoCategoryId", youTubeDTO.getVideoCategoryId())
+                            .queryParam("key", YOUTUBE_KEY)
+                            .build())
+                    .retrieve().toEntity(String.class).block();
 
 
-            RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
-
-            log.info("status code : {}", response.getStatusCode());
+            log.info("status : {}", response.getStatusCode());
             log.info("body : {}", response.getBody());
 
             return ResponseEntity.ok().body(response.getBody());
